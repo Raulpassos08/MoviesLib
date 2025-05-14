85fe3e611b334771f4ca493777c64b6fcc803313
raulpassos08 <raulfera2@gmail.com>
2025-05-14 20:18:46 -0300

    Complete home and initial css

--- src/componentes/Navbar.jsx
+++ src/componentes/Navbar.jsx
@@ -1,21 +1,25 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
-import { BiCameraMovie, BiSearchAlt2 } from "react-icons/bi"
+import React from "react";
+import { Link } from "react-router-dom";
+import { BiCameraMovie, BiSearchAlt2 } from "react-icons/bi";
+
+import "./navbar.css";
 
 const Navbar = () => {
   return (
-    <nav id='navbar'>
-        <h2>
-            <Link to="/"><BiCameraMovie /> MoviesLib</Link>
-        </h2>
-        <form>
-            <input type="text" placeholder='Busque um filme'/>
-            <button type='submit'>
-            <BiSearchAlt2 /> 
-            </button>
-        </form>
+    <nav id="navbar">
+      <h2>
+        <Link to="/">
+          <BiCameraMovie /> MoviesLib
+        </Link>
+      </h2>
+      <form>
+        <input type="text" placeholder="Busque um filme" />
+        <button type="submit">
+          <BiSearchAlt2 />
+        </button>
+      </form>
     </nav>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,40 +1,37 @@
-import React from 'react'
-import { useState, useEffect } from 'react'
-import { MOCK_MOVIES_TOP_20 } from '../db'
+import React from "react";
+import { useState, useEffect } from "react";
+import MovieCard from "../componentes/MovieCard";
 
-const moviesUrl = import.meta.env.VITE_API
-const apiKey = import.meta.env.VITE_API_KEY
+import "./MovieGrid.css";
+
+const moviesUrl = import.meta.env.VITE_API;
+const apiKey = import.meta.env.VITE_API_KEY;
 
 const Home = () => {
-  const [topMovies, setTopMovies] = useState([])
+  const [topMovies, setTopMovies] = useState([]);
 
   const getTopRatedMovies = async (url) => {
-    // const res = await fetch(url);
-    // const data = await res.json();
-
-    const movies = MOCK_MOVIES_TOP_20
-
-    console.log(movies)
-
-    setTopMovies(movies)
-  
+    const res = await fetch(url);
+    const data = await res.json();
+    setTopMovies(data.results);
   };
 
   useEffect(() => {
     const topRatedUrl = `${moviesUrl}top_rated?${apiKey}`;
-
-    getTopRatedMovies(topRatedUrl)
+    console.log(topRatedUrl);
+    getTopRatedMovies(topRatedUrl);
   }, []);
 
   return (
-    <div className='container'>
-      <h2 className='title'>Melhores Filmes</h2>
+    <div className="container">
+      <h2 className="title">Melhores Filmes</h2>
       <div className="movies-container">
         {topMovies.length === 0 && <p>Carregando...</p>}
-        {topMovies.length > 0 && topMovies.map((movie) => <p>{movie.title}</p>)}
+        {topMovies.length > 0 &&
+          topMovies.map((movie) => <MovieCard key={movie.id} movie={movie} />)}
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
